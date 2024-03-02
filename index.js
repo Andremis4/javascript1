362b34a48db088e2305b16fb630f086c19be4c3a
Andremisa <andreaboadas3@gmail.com>
2024-03-02 16:31:38 -0300

    desafiojava

---
+++ index.js
@@ -0,0 +1,13 @@
+precio = 400000;
+let cantidadInicial = 1;
+
+let precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+let cantidad = document.querySelector(".cantidad");
+cantidad.innerHTML = cantidadInicial;
+
+let total = document.querySelector(".valor-total");
+total.innerHTML = precio;
+
+
